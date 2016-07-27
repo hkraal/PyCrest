756b40c44e6b45245c174d311b8995e06d88b4f7
Henk Kraal <gitlab.com@spenkman.nl>
2016-07-27 19:26:54 +0200

    pervese unittest

--- tests/test_pull_37.py
+++ tests/test_pull_37.py
@@ -17,6 +17,9 @@ def root_mock(url, request):
         content='''{
     "marketData": {
         "href": "https://crest-tq.eveonline.com/market/prices/"
+    },
+    "perverse": {
+        "href": "https://crest-tq.eveonline.com/perverse/"
     }}''', headers={"content-type": "application/vnd.ccp.eve.Api-v5+json; charset=utf-8"})
 
 
@@ -31,10 +34,33 @@ def market_prices_mock(url, request):
         'geCount_str": "1", "totalCount": 10213}',
         headers={"content-type": "application/vnd.ccp.eve.Api-v5+json; charset=utf-8"})
 
+@httmock.urlmatch(
+    scheme="https",
+    netloc=r"(api-sisi\.test)?(crest-tq\.)?eveonline\.com$",
+    path=r"^/perverse/?$")
+def perverse_mock(url, request):
+    return httmock.response(
+        status_code=200,
+        content='''{
+    "result": {
+        "result": "10213",
+        "status_code": 500
+    },
+    "items": [],
+    "status_code": {
+        "result": "10214",
+        "status_code": 501
+    },
+    "pageCount_str": "1",
+    "totalCount": 10213
+}''',
+        headers={"content-type": "application/vnd.ccp.eve.Api-v5+json; charset=utf-8"})
+
 
 all_httmocks = [
     root_mock,
-    market_prices_mock]
+    market_prices_mock,
+    perverse_mock]
 
 
 class TestEVE(unittest.TestCase):
@@ -56,8 +82,19 @@ class TestEVE(unittest.TestCase):
             self.assertEqual(res.result.status_code, 500)
             self.assertEqual(res.result.result, '10213')
 
-
-
+    def test_perverse(self):
+        with httmock.HTTMock(*all_httmocks):
+            res = self.api().perverse()
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(res.pageCount_str, '1')
+            self.assertEqual(res.result.pageCount_str, '1')
+            self.assertEqual(res.totalCount, 10213)
+            self.assertEqual(res.result.totalCount, 10213)
+            self.assertEqual(res.items, [])
+            self.assertEqual(res.result.items, [])
+            self.assertEqual(res.result.status_code.status_code, 501)
+            self.assertEqual(res.result.status_code.result, '10214')
+            self.assertEqual(res.result.result.result, '10213')
 
 if __name__ == "__main__":
     unittest.main()
